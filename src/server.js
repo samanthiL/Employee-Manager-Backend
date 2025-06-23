26516e8b159b7c6ae8207d34d09d00c6ec85010f
samanthiL <samanthilw4@gmail.com>
2025-06-24 01:11:58 +0530

    env file changes

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/employee';
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
  mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
